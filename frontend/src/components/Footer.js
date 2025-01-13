6ffd6600ade64ad488335ee2013c4daa0151b3d8
Anan328 <ananshah328@gmail.com>
2025-01-13 15:16:37 +0530

    minor changes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,25 +1,23 @@
 import React, { useState } from "react";
 import { FaTwitter, FaLinkedin, FaFacebook, FaInstagram } from "react-icons/fa";
 import "../style.css";
-import {postEmail} from '../services/service.js'
+import { postEmail } from "../services/service.js";
 function Footer() {
+  const [email, setEmail] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEmail("");
+    postEmail({ email })
+      .then((response) => {
+        console.log(response.data);
+        alert("Subscribed successfully!");
+      })
 
-    const [email, setEmail] = useState('');
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-        setEmail('');
-        postEmail({email})
-        .then((response)=>{
-            console.log(response.data);
-            alert("Subscribed successfully!");
-        })
-
-        .catch(err=>{
-            console.error(err);
-            alert("An error occurred while submitting the form.");
-            
-        })
-    }
+      .catch((err) => {
+        console.error(err);
+        alert("An error occurred while submitting the form.");
+      });
+  };
   const currentYear = new Date().getFullYear();
   return (
     <footer className="footer">
@@ -53,9 +51,15 @@ function Footer() {
         &copy; {currentYear} Open-Hub-Community, All rights reserved.
       </p>
       <form onSubmit={handleSubmit} className="newsletter-form">
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email for NewsLetter" required />
-          <button type="submit">Send</button>
-        </form>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter your email for NewsLetter"
+          required
+        />
+        <button type="submit">Send</button>
+      </form>
     </footer>
   );
 }
