21a0ad83ee9cc43a7fc147de3139d3b6c009f62c
Anan328 <ananshah328@gmail.com>
2025-01-14 00:53:09 +0530

    backend

--- frontend/src/services/service.js
+++ frontend/src/services/service.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const api  = axios.create({
-    baseURL: "https://f050-117-96-40-84.ngrok-free.app" // "https://jsonplaceholder.typicode.com" // test api
+    baseURL:  "https://jsonplaceholder.typicode.com" // test api
 })
 
-const postEmail = (email) => api.post("/register",email); // /register
-const postForm = (details) => api.post("/posts",details);
+const postEmail = (email) => api.post("/posts",email); //  /register
+const postForm = (details) => api.post("/posts",details); // /register
 
 export {postEmail,postForm}
