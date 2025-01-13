b3fc0edc1da3a6501b6018527e39e2d3d389432d
Anan328 <ananshah328@gmail.com>
2025-01-13 12:41:38 +0530

    set Navbar and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router , Routes, Route} from 'react-router-dom';
+import './style.css'
+import SubmitProject from './components/SubmitProject';
+import About from './components/About';
+
 
 function App() {
   return (
-    <></>
+    <div className='container'>
+    <Router>
+    <Navbar/>
+    <Routes>
+      <Route path='/submit-project' element={<SubmitProject/>}/>
+      <Route path='/about' element={<About/>}/>
+    </Routes>
+    </Router>
+    </div>
   );
 }
 
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from '../assets/logo.ico';
+
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div>
+        <img src= {logo} alt="Hackathon Logo" className="logo-image" />
+          
+        </div>
+        <div className="navbar-links">
+          <Link to="/">Home</Link>
+          <Link to="/submit-project">Submit Project</Link>
+          <Link to="/about">About</Link>
+          <Link to="/register">
+          <button className="register-button">Register Now</button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
