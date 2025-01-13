b01f0b6276299f86f2ec90b4550fae6ad620cbb0
Naik Mubashir <naikmubashir095@gmail.com>
2025-01-14 00:32:04 +0530

    Add cors middleware

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import connectDb from './config/DBconn.js';
+import cors from 'cors'
 import register from './routes/registerRoute.js';
 const app=express();
 const PORT=process.env.PORT ||8000;
 
-// app.use(cors());
-// app.use( express.urlencoded({extended:false}) );
+app.use(cors());
+app.use( express.urlencoded({extended:false}) );
 app.use(express.json());
 
 app.use('/register',register);
