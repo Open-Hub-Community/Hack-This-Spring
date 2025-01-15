fd82f6ac47f1f8f731e0680b9e06901ba55655bd
Naik Mubashir <naikmubashir095@gmail.com>
2025-01-16 00:22:55 +0530

    Fix bugs

--- backend/controllers/registerController.js
+++ backend/controllers/registerController.js
@@ -32,8 +32,26 @@ const handleNewRegistration = async (req, res) => {
         const newUser = await User.create({ name, email, phoneNumber, college, semester, gender, agree });
 
         console.log('New user created:', newUser);
+
+
+        const mailOptions={
+            from:'"Hack-This-Spring Team" open8hub@gmail.com',
+            to: email,
+            subject: 'Registration Confirmation for Hack This Spring',
+            text:`Dear ${name},\n\nThank you for registering for HackThisSpring! We are excited to have you onboard for this prestigious hackathon organized by the OpenHub Community.\n\nIn the coming days, we will be hosting a team-building meet where you will have the opportunity to connect with other developers, discuss ideas, and form your teams for the event. We encourage you to participate actively and collaborate with fellow developers.\n\nWe look forward to seeing you at HackThisSpring and wish you the best of luck!\n\nBest regards,\nThe HackThisSpring Team`,
+            html:`
+                <p>Dear <strong>${name}</strong>,</p>
+                <p>Thank you for registering for <strong>HackThisSpring</strong>! We are thrilled to have you onboard for this exciting hackathon, organized by the <strong>OpenHub Community</strong>.</p>
+                <p>In the coming days, we will be hosting a <strong>team-building meet</strong> where you will have the opportunity to connect with other developers, exchange ideas, and form your teams for the event. We encourage you to actively participate and collaborate with your fellow developers.</p>
+                <p>We are excited about your participation in <strong>HackThisSpring</strong> and wish you the best of luck as you prepare for the hackathon.</p>
+                <p>Best regards,</p>
+                <p>The HackThisSpring Team</p>
+            `
+        };
+        
+
         // Send confirmation email
-        //await sendConfirmationEmail(name, email);
+        await sendConfirmationEmail( email, mailOptions);
         return res.status(201).json({
             message: `New user '${newUser.name}' created successfully.`//, user: newUser, // Include the created user details if needed
         });
--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer'
 
-const sendConfirmationEmail=async(name, email)=>{
+const sendConfirmationEmail=async( email,mailOptions)=>{
     try{
         const transporter= nodemailer.createTransport({
             // host:"smtp.ethereal.email",
@@ -12,26 +12,9 @@ const sendConfirmationEmail=async(name, email)=>{
                 pass:process.env.EMAIL_PASSWORD
             }
         });
-
-        const mailOptions={
-            from:'"Hack-This-Spring Team" open8hub@gmail.com',
-            to: email,
-            subject: 'Registration Confirmation for Hack This Spring',
-            text:`Dear ${name},\n\nThank you for registering for HackThisSpring! We are excited to have you onboard for this prestigious hackathon organized by the OpenHub Community.\n\nIn the coming days, we will be hosting a team-building meet where you will have the opportunity to connect with other developers, discuss ideas, and form your teams for the event. We encourage you to participate actively and collaborate with fellow developers.\n\nWe look forward to seeing you at HackThisSpring and wish you the best of luck!\n\nBest regards,\nThe HackThisSpring Team`,
-            html:`
-                <p>Dear <strong>${name}</strong>,</p>
-                <p>Thank you for registering for <strong>HackThisSpring</strong>! We are thrilled to have you onboard for this exciting hackathon, organized by the <strong>OpenHub Community</strong>.</p>
-                <p>In the coming days, we will be hosting a <strong>team-building meet</strong> where you will have the opportunity to connect with other developers, exchange ideas, and form your teams for the event. We encourage you to actively participate and collaborate with your fellow developers.</p>
-                <p>We are excited about your participation in <strong>HackThisSpring</strong> and wish you the best of luck as you prepare for the hackathon.</p>
-                <p>Best regards,</p>
-                <p>The HackThisSpring Team</p>
-            `
-        };
-
         //Send mail
         await transporter.sendMail(mailOptions);
         console.log(`Confirmation email sent to ${email}`);
-
     }
     catch (error) {
         console.error('Error sending confirmation email:', error.message);
