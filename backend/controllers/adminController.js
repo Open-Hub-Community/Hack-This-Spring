25edb024cca12d8ef8783cd093b4d8de009e11b6
Naik Mubashir <naikmubashir095@gmail.com>
2025-01-16 22:57:15 +0530

    Refactor database queries to run in parallel using Promise.all

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -3,10 +3,10 @@ import Subscriber from "../models/Subscriber.js";
 
 const getAllStudents=async (req,res)=>{
     try {
-        const users = await User.find(); // Fetch all registered users
-        console.log(users)
-        const subscribers = await Subscriber.find(); // Fetch all registered users
-        console.log(subscribers)
+        const [users, subscribers] = await Promise.all([
+            User.find(), // Fetch all registered users
+            Subscriber.find() // Fetch all subscribers
+        ]);
         res.json([users?.length ? users : [], subscribers?.length ? subscribers : []]);
     } catch (err) {
         res.status(500).json({ message: 'Server error-Couldnt fetch the students', error: err.message });
